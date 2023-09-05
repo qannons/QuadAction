b4d57df02024b3acb9f144f588f1b05bc19e1394
qannons <47106579+qannons@users.noreply.github.com>
2023-09-05 10:53:53 +0900

    B39(미완)
    플레이어 점프

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,23 +10,45 @@ public class Player : MonoBehaviour
     float hAxis;
     float vAxis;
     bool runDown;
+    bool jumpDown;
+    bool isJump;
+
     Vector3 moveVec;
 
+    Rigidbody rb;
     Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        InputUpdate();
+
+        MoveUpdate();
+
+        //방향에 따라 회전
+        transform.LookAt(moveVec + transform.position);
+
+        Jump();
+    }
+
+    void InputUpdate()
+    {
         //Axis값을 정수로 반환하는 함수
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
-        runDown = Input.GetButton("Run");
 
+        runDown = Input.GetButton("Run");
+        jumpDown = Input.GetButtonDown("Jump");
+    }
+
+    void MoveUpdate()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
         transform.position += moveVec * speed * (runDown ? 1.5f : 1.0f) * Time.deltaTime;
@@ -36,8 +58,28 @@ public class Player : MonoBehaviour
 
         //Shift 같이 누르면 달림
         animator.SetBool("isRun", runDown);
+    }
 
-        transform.LookAt(moveVec + transform.position);
+    void Jump()
+    {
+        if (jumpDown && isJump == false) 
+        {
+            rb.AddForce(Vector3.up * 15, ForceMode.Impulse);
+             
+            animator.SetBool("isJump", true);
+            animator.SetTrigger("doJump");
 
+            isJump = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //태그를 활용해 바닥에만 작동하도록
+        if (collision.gameObject.tag == "Floor")
+        {
+            isJump = false;
+            animator.SetBool("isJump", false);
+        }
     }
 }
