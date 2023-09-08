753d049598255c024b3d86e5e4ce516ba972bbc5
qannons <emforhs0913@naver.com>
2023-09-08 21:08:11 +0900

    B41(완)
    드랍 무기 입수와 교체

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -22,11 +23,15 @@ public class Player : MonoBehaviour
     bool isRun;
     bool isJump;
     bool isDodge;
+    bool isSwap;
 
-    //temp start
+    bool swapDown1;
+    bool swapDown2;
+    bool swapDown3;
+
     float curTime = 0;
     Vector3 prevMoveVec;
-    //temp end
+    
     Vector3 moveVec;
 
     Rigidbody rb;
@@ -34,6 +39,8 @@ public class Player : MonoBehaviour
 
     //트리거된 아이템을 저장하기 위한 변수 선언
     GameObject nearObject;
+
+    int  equipWeaponIndex = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -56,6 +63,7 @@ public class Player : MonoBehaviour
         Jump();
         Dodge();
         Interaction();
+        SwapWeapon();
     }
 
     void InputUpdate()
@@ -69,50 +77,23 @@ public class Player : MonoBehaviour
 
         jumpDown = Input.GetButtonDown("Jump");
         interactDown = Input.GetButtonDown("Interaction");
-    }
-    bool CanRun()
-    {
-        //(-1, 0) -> (-1, 0) == o
-        //(-0.7, 0.7) -> (-1, 0) == o
-        //(0.7, 0.7) -> (-0.7, 0.7) == x
-        //(-0.7, 0.7) -> (0, -1) == o
-        if (prevMoveVec.x >= 0)
-        {
-            if(moveVec.x >= 0)
-                return true;
-            return false;
-        }
-        else if (prevMoveVec.x < 0)
-        {
-            if (moveVec.x <= 0)
-                return true;
-            return false;
-        }
 
-        if (prevMoveVec.z >= 0)
-        {
-            if (moveVec.z >= 0)
-                return true;
-            return false;
-        }
-        else if (prevMoveVec.z < 0)
-        {
-            if (moveVec.z <= 0)
-                return true;
-            return false;
-        }
-        return true;
+        swapDown1 = Input.GetButtonDown("Swap1");
+        swapDown2 = Input.GetButtonDown("Swap2");
+        swapDown3 = Input.GetButtonDown("Swap3");
+
+        //Debug.Log(swapDown1);
     }
+    
     void MoveUpdate()
     {
-        float deltaTime = Time.deltaTime;
-
+        prevMoveVec = moveVec;
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
-        if(prevMoveVec != Vector3.zero && CanRun())
+        //한 방향으로 이동하면 달리기 모드
+        if (prevMoveVec != Vector3.zero && isDodge == false && prevMoveVec == moveVec)
         {
-            Debug.Log("true");
-            curTime += deltaTime;
-            if (curTime > 1.0f) 
+            curTime += Time.deltaTime; ;
+            if (curTime > 0.5f) 
             {
                 animator.SetBool("isRun", true);
                 isRun = true;
@@ -120,23 +101,42 @@ public class Player : MonoBehaviour
         }
         else
         {
-            Debug.Log("false");
             curTime = 0;
             animator.SetBool("isRun", false);
             isRun = false;
         }
-
-        transform.position += moveVec * speed * (isRun ? 3.5f : 1.0f) * deltaTime;
+        
+        transform.position += moveVec * speed * (isRun ? 2f : 1.0f) * Time.deltaTime; ;
 
         //방향키 누르면 걸음
-        animator.SetBool("isWalk", moveVec != Vector3.zero);
-
-        //Shift 같이 누르면 달림
-        //animator.SetBool("isRun", isRun);
-
-        prevMoveVec = moveVec;
+        animator.SetBool("isWalk", moveVec != Vector3.zero);;
     }
 
+    void SwapWeapon()
+    {
+        int weaponIndex = -1;
+
+        if (swapDown1 && hasWeapons[0])
+            weaponIndex = 0;
+        else if (swapDown2 && hasWeapons[1])
+            weaponIndex = 1;
+        else if (swapDown3 && hasWeapons[2])
+            weaponIndex = 2;
+        else
+            return;
+
+        if (weaponIndex == equipWeaponIndex)
+            return;
+
+        if (equipWeaponIndex >= 0)
+            weapons[equipWeaponIndex].SetActive(false);
+
+        equipWeaponIndex = weaponIndex;
+        weapons[equipWeaponIndex].SetActive(true);
+
+        animator.SetTrigger("doSwap");
+        
+    }
     void Jump()
     {
         if (jumpDown && moveVec == Vector3.zero && isJump == false) 
