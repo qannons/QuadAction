86dae7661f23b2bc69d6082b3bf6fe76271ec726
조정현 <emforhs0913@naver.com>
2023-10-13 16:44:56 +0900

    1013
    진행 중

---
+++ Assets/myGrid.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myGrid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = transform.childCount;
+        Debug.Log(a);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
