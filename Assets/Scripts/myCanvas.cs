46b4bfb8ab760598c9d8e02c4c3358a1db0dc088
YSM <clov7153@gmail.com>
2023-12-01 16:20:39 +0900

    12-01-4:20

---
+++ Assets/Scripts/myCanvas.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myCanvas : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
