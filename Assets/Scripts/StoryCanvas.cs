b67da64c4004848e87ef01bf06e2bdfa59b23dfb
ysm <clov7153@gmail.com>
2023-12-10 23:04:01 +0900

    11111111

---
+++ Assets/Scripts/StoryCanvas.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryCanvas : MonoBehaviour
+{
+    private Player player;
+    public GameObject storyCanvas;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()
+    }
+
+    public void OpenStoryCanvas()
+    {
+        player.canMove = false;
+        storyCanvas.SetActive(true);
+    }
+    
+    public void CloseStoryCanvas()
+    {
+        player.canMove = true;
+        storyCanvas.SetActive(true);
+    }
+    
+}
